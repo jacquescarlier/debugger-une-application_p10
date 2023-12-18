623b0b404df7e7e163b01bdc35afd3699fd142f4
jacques <lapitou60@gmail.com>
2023-12-18 14:51:36 +0100

    comment on the choice of method

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -20,18 +20,27 @@ const EventList = () => {
       ? data?.events.filter(event => event.type === type)
       : data?.events) || [];
  
-// apply pagination on filtered events
+/* La méthode slice est utilisée pour extraire une partie du tableau filteredEvents pour la page actuelle. Voici une répartition des paramètres transmis à slice :
+
+L'indice de départ est calculé comme (currentPage - 1) * PER_PAGE. En effet, les indices de tableau sont de base zéro, donc si vous êtes sur la première page (currentPage vaut 1), vous voulez commencer à partir de l'index 0. Si vous êtes sur la deuxième page, vous voulez pour commencer à partir de l'index PER_PAGE, et ainsi de suite.
+
+L'indice de fin est calculé comme currentPage * PER_PAGE. Cela détermine où la tranche doit se terminer. Par exemple, si PER_PAGE vaut 10 et currentPage vaut 2, alors l'index de fin serait 20, ce qui signifie que vous souhaitez obtenir les éléments de l'index 10 à 19.
+*/
     const pagination = filteredEvents.slice(
       (currentPage - 1) * PER_PAGE,
       currentPage * PER_PAGE
     );
-                           
+          console.log("pagination", pagination)                 
      const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
-    // const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) ;
-   // console.log("floor", Math.floor((filteredEvents?.length || 0) / PER_PAGE) )
-   // console.log("ceil", Math.ceil(filteredEvents.length / PER_PAGE))
-   // console.log("filtered", filteredEvents.length)
-   //  console.log("perpage", PER_PAGE)
+    // floor renvoie le plus grand entier qui est inférieur ou égal à un nombre
+    // ceil retourne le plus petit entier supérieur ou égal au nombre donné.
+    console.log("currentPage", currentPage)
+    console.log("filteredEvents", filteredEvents)
+    console.log("floor",(Math.floor((filteredEvents?.length || 0) / PER_PAGE) +1))
+    // même résultat avec trunc, round
+    console.log("ceil", Math.ceil(filteredEvents.length / PER_PAGE))
+    console.log("filtered", filteredEvents.length)
+  
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
