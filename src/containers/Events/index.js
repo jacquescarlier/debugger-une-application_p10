09747fc03c61291ab21ae894426076e54cbf8d5a
jacques <lapitou60@gmail.com>
2023-12-20 15:04:46 +0100

    ppagination

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,42 +14,37 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
 
-// filter events based on selected type
+  // filter events based on selected type
   const filteredEvents = (
     type
       ? data?.events.filter(event => event.type === type)
       : data?.events) || [];
- 
 
-      //
-/* La méthode slice est utilisée pour extraire une partie du tableau filteredEvents pour la page actuelle. Voici une répartition des paramètres transmis à slice :
 
-L'indice de départ est calculé comme (currentPage - 1) * PER_PAGE. En effet, les indices de tableau sont de base zéro, donc si vous êtes sur la première page (currentPage vaut 1), vous voulez commencer à partir de l'index 0. Si vous êtes sur la deuxième page, vous voulez pour commencer à partir de l'index PER_PAGE, et ainsi de suite.
-
-L'indice de fin est calculé comme currentPage * PER_PAGE. Cela détermine où la tranche doit se terminer. Par exemple, si PER_PAGE vaut 10 et currentPage vaut 2, alors l'index de fin serait 20, ce qui signifie que vous souhaitez obtenir les éléments de l'index 10 à 19.
-*/
-    const pagination = filteredEvents.slice(
-      (currentPage - 1) * PER_PAGE,
-      currentPage * PER_PAGE
-    );
-          console.log("pagination", pagination)                 
-     const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
-    // floor renvoie le plus grand entier qui est inférieur ou égal à un nombre
-    // ceil retourne le plus petit entier supérieur ou égal au nombre donné.
-    console.log("currentPage", currentPage)
-    console.log("filteredEvents", filteredEvents)
-    console.log("floor",(Math.floor((filteredEvents?.length || 0) / PER_PAGE) +1))
-    // même résultat avec trunc, round
-    console.log("ceil", Math.ceil(filteredEvents.length / PER_PAGE))
-    console.log("filtered", filteredEvents.length)
+  //
+  /* La méthode slice est utilisée pour extraire une partie du tableau filteredEvents pour la page actuelle. Voici une répartition des paramètres transmis à slice :
   
+  L'indice de départ est calculé comme (currentPage - 1) * PER_PAGE. En effet, les indices de tableau sont de base zéro, donc si vous êtes sur la première page (currentPage vaut 1), vous voulez commencer à partir de l'index 0. Si vous êtes sur la deuxième page, vous voulez pour commencer à partir de l'index PER_PAGE, et ainsi de suite.
+  
+  L'indice de fin est calculé comme currentPage * PER_PAGE. Cela détermine où la tranche doit se terminer. Par exemple, si PER_PAGE vaut 10 et currentPage vaut 2, alors l'index de fin serait 20, ce qui signifie que vous souhaitez obtenir les éléments de l'index 10 à 19.
+  */
+  const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
+  // floor renvoie le plus grand entier qui est inférieur ou égal à un nombre
+  // ceil retourne le plus petit entier supérieur ou égal au nombre donné.
+  console.log("currentPage", currentPage)
+  console.log("filteredEvents", filteredEvents)
+  console.log("floor", (Math.floor((filteredEvents.length) / PER_PAGE) + 1))
+  // même résultat avec trunc, round
+  console.log("ceil", Math.ceil(filteredEvents.length / PER_PAGE))
+  console.log("filtered", filteredEvents.length)
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
 
   const typeList = new Set(data?.events.map((event) => event.type));
-  
+
   return (
     <>
       {error && <div>An error occured</div>}
@@ -63,7 +58,10 @@ L'indice de fin est calculé comme currentPage * PER_PAGE. Cela détermine où l
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
           <div id="events" className="ListContainer">
-            {pagination.map((event) => (
+            {(filteredEvents.slice(
+              (currentPage - 1) * PER_PAGE,
+              currentPage * PER_PAGE
+            )).map((event) => (
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
                   <EventCard
