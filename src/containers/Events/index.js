54465fc8d22cdaa44d63231c0938095cf5ff1724
jacques <lapitou60@gmail.com>
2023-12-21 17:03:10 +0100

    end day backup

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,32 +13,28 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-
+  const startIndex = (currentPage - 1) * PER_PAGE;
+  const endIndex = currentPage * PER_PAGE;
   const filteredEvents = (
     (!type
       ? data?.events
       : data?.events) || []
   ).filter((event) => {
-    if ((!type || event.type === type)
-    // && 
-   // (currentPage - 1) * PER_PAGE <= index &&
-   // PER_PAGE * currentPage > index
-    ) {
+    if ((!type || event.type === type)) 
+    {
       return true;
     }
     return false;
   });
-// {filteredEvents.map((event) => (
-//  <Modal key={event.id} Content={<ModalEvent event={event} />}>
-//  {({ setIsOpened }) =>
+  // {filteredEvents.map((event) => (
+  //  <Modal key={event.id} Content={<ModalEvent event={event} />}>
+  //  {({ setIsOpened }) =>
   // j'ai retiré l'utilisation de cette fonction qui pourtant optimise le code
   /* La méthode slice est utilisée pour extraire une partie du tableau filteredEvents pour la page actuelle. Voici une répartition des paramètres transmis à slice :
   L'indice de départ est calculé comme (currentPage - 1) * PER_PAGE. En effet, les indices de tableau sont de base zéro, donc si vous êtes sur la première page (currentPage vaut 1), vous voulez commencer à partir de l'index 0. Si vous êtes sur la deuxième page, vous voulez pour commencer à partir de l'index PER_PAGE, et ainsi de suite.
   L'indice de fin est calculé comme currentPage * PER_PAGE. Cela détermine où la tranche doit se terminer. Par exemple, si PER_PAGE vaut 10 et currentPage vaut 2, alors l'index de fin serait 20, ce qui signifie que vous souhaitez obtenir les éléments de l'index 10 à 19.
   */
-  const startIndex = (currentPage - 1) * PER_PAGE;
-  const endIndex = currentPage * PER_PAGE;
-
+  const eventsForCurrentPage = filteredEvents.slice(startIndex, endIndex)
   const typeList = new Set(data?.events.map((event) => event.type));
   const pageNumber = Math.ceil((filteredEvents?.length || 0) / PER_PAGE);
   // const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
@@ -62,8 +58,7 @@ const EventList = () => {
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
           <div id="events" className="ListContainer">
-            {(filteredEvents.slice(startIndex,
-              endIndex)).map((event) => (
+            {(eventsForCurrentPage).map((event) => (
                 <Modal key={event.id} Content={<ModalEvent event={event} />}>
                   {({ setIsOpened }) => (
                     <EventCard
