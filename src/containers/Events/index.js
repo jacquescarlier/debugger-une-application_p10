8fe9e9413c4d959995b0dba584403894bba6beb0
jacques <lapitou60@gmail.com>
2023-12-26 13:53:04 +0100

    backup

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -28,18 +28,18 @@ const EventList = () => {
   /* La méthode slice est utilisée pour extraire une partie du tableau filteredEvents pour la page actuelle. Voici une répartition des paramètres transmis à slice :
   L'indice de départ est calculé comme (currentPage - 1) * PER_PAGE. En effet, les indices de tableau sont de base zéro, donc si vous êtes sur la première page (currentPage vaut 1), vous voulez commencer à partir de l'index 0. Si vous êtes sur la deuxième page, vous voulez pour commencer à partir de l'index PER_PAGE, et ainsi de suite.
   L'indice de fin est calculé comme currentPage * PER_PAGE. Cela détermine où la tranche doit se terminer. Par exemple, si PER_PAGE vaut 10 et currentPage vaut 2, alors l'index de fin serait 20, ce qui signifie que vous souhaitez obtenir les éléments de l'index 10 à 19.
+  index de 0 à 8 par page = 9 éléments page 1 startIndex 0 endIndex 9
+  page 2 start index 9 endIndex 18
   */
   const eventsForCurrentPage = filteredEvents.slice(startIndex, endIndex)
   const typeList = new Set(data?.events.map((event) => event.type));
-  const pageNumber = Math.ceil((filteredEvents?.length || 0) / PER_PAGE);
-  // const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  const numberOfPages = Math.ceil((filteredEvents?.length || 0) / PER_PAGE);
   // La fonction Math.ceil() retourne le plus petit entier supérieur ou égal au nombre donné. Plus facile à gérer dans notre cas que Math.floor
   // alors que La fonction Math.floor(x) renvoie le plus grand entier qui est inférieur ou égal à un nombre x.
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-
   return (
     <>
       {error && <div>An error occured</div>}
@@ -68,7 +68,7 @@ const EventList = () => {
             ))}
           </div>
           <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
+            {[...Array(numberOfPages || 0)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
               <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
