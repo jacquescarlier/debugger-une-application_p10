570271787e34786c59ef53677372ac5d93acf50d
jacques <lapitou60@gmail.com>
2024-01-02 12:55:05 +0100

    midday backup

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,6 @@ const EventList = () => {
 
   const startIndex = (currentPage - 1) * PER_PAGE;
   const endIndex = currentPage * PER_PAGE;
-  
   const filteredEvents = (
     (!type ? data?.events : data?.events) || []
   ).filter((event) => {
@@ -28,8 +27,8 @@ const EventList = () => {
   /* La méthode slice est utilisée pour extraire une partie du tableau filteredEvents pour la page actuelle. Voici une répartition des paramètres transmis à slice :
   L'indice de départ est calculé comme (currentPage - 1) * PER_PAGE. En effet, les indices de tableau sont de base zéro, donc si vous êtes sur la première page (currentPage vaut 1), vous voulez commencer à partir de l'index 0. Si vous êtes sur la deuxième page, vous voulez pour commencer à partir de l'index PER_PAGE, et ainsi de suite.
   L'indice de fin est calculé comme currentPage * PER_PAGE. Cela détermine où la tranche doit se terminer. Par exemple, si PER_PAGE vaut 9 et currentPage vaut 2, alors l'index de fin serait 18, ce qui signifie que vous souhaitez obtenir les éléments de l'index 9 à 18.
-  index de 0 à 8 par page = 9 éléments page 1 startIndex 0 endIndex 8
-  page 2 start index 9 endIndex 17
+  index de 0 à 9 par page = 9 éléments page 1 startIndex 0 endIndex 9
+  page 2 start index 9 endIndex 18
   */
   const eventsForCurrentPage = filteredEvents.slice(startIndex, endIndex)
   const typeList = new Set(data?.events.map((event) => event.type));
